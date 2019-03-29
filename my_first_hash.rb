7ef02e72b940c6ecd8d7c805e90adff0cdfa0108
Luis Hong <luishong50@gmail.com>
2019-03-29 20:25:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,6 +32,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 shipping_manifest["musket"]=>2
+                 shipping_manifest["gun powder"]=>4
   # add 2 muskets to the shipping_manifest hash below
 
 
